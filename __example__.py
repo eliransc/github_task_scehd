d97c3523e37506a50178777d492ae78a17424348
eliransc <scherzter@gmail.com>
2022-07-05 15:44:15 -0400

    curr_num modification

--- __example__.py
+++ __example__.py
@@ -451,7 +451,7 @@ def main(args):
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file_path', type=str, help='which settings are used', default='Result_table.pkl')
+    parser.add_argument('--file_path', type=str, help='which settings are used', default='Result_table_1.pkl')
     parser.add_argument('--num_iter', type=int, help='how many iterations we run', default=50)
     args = parser.parse_args(argv)
 
