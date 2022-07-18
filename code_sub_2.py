879eb8f6ee8719cd37f4935f5337e77d392c1d36
eliransc <scherzter@gmail.com>
2022-07-18 15:13:23 -0400

    ljnrjkev

--- code_sub_2.py
+++ code_sub_2.py
@@ -559,7 +559,7 @@ def main(args):
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file_path', type=str, help='which settings are used', default='Result_table_6.pkl')
+    parser.add_argument('--file_path', type=str, help='which settings are used', default='Result_table_7.pkl')
     parser.add_argument('--num_iter', type=int, help='how many iterations we run', default=5)
     args = parser.parse_args(argv)
 
